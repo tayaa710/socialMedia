7db55b6cb6cf1cd1580e56616b449ecc393b9a10
tayaa710 <154112253+tayaa710@users.noreply.github.com>
2025-02-21 19:28:37 +1300

    Got layout working

--- mvpFrontend/src/components/profilePosts/ProfilePosts.jsx
+++ mvpFrontend/src/components/profilePosts/ProfilePosts.jsx
@@ -1,62 +1,76 @@
+
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import Post from "../post/Post";
-import './profilePosts.css'
+import "./profilePosts.css";
 
 export default function ProfilePosts() {
-
   const posts = [
     {
       user: "Aaron",
       date: "20/02/2025",
       likes: 100,
-      Caption: "Photo 1",
+      caption: "Photo 1",
       url: "../../assets/postReal/1.JPG",
-      id:1
+      id: 1,
     },
     {
       user: "Aaron",
       date: "20/02/2025",
       likes: 1030,
-      Caption: "Photo 2",
+      caption: "Photo 2",
       url: "../../assets/postReal/2.jpeg",
-      id:2
+      id: 2,
     },
     {
       user: "Aaron",
       date: "20/02/2025",
       likes: 100,
-      Caption: "Photo 3",
+      caption: "Photo 3",
       url: "../../assets/postReal/3.jpeg",
-      id:3
+      id: 3,
     },
     {
       user: "Aaron",
       date: "20/02/2025",
       likes: 100,
-      Caption: "Photo 4",
+      caption: "Photo 4",
       url: "../../assets/postReal/4.jpeg",
-      id:4
+      id: 4,
     },
+    {
+      user: "Aaron",
+      date: "20/02/2025",
+      likes: 100,
+      caption: "GITHYB TESTTTTTTTTTTTTTTT",
+      url: "../../assets/postReal/4.jpeg",
+      id: 7,
+    },
+    {
+      user: "Aaron",
+      date: "20/02/2025",
+      likes: 100,
+      caption: "GITHYB TESTTTTTTTTTTTTTTT",
+      url: "../../assets/postReal/4.jpeg",
+      id: 8,
+    },
+  ];
 
-    {
-      user: "Aaron",
-      date: "20/02/2025",
-      likes: 100,
-      Caption: "GITHYB TESTTTTTTTTTTTTTTT",
-      url: "../../assets/postReal/4.jpeg",
-      id:7
-    },
-    {
-      user: "Aaron",
-      date: "20/02/2025",
-      likes: 100,
-      Caption: "GITHYB TESTTTTTTTTTTTTTTT",
-      url: "../../assets/postReal/4.jpeg",
-      id:8
-    }
-  ]
   return (
-   <div className="postsContainer">
-     {posts.map(post => <Post date={post.date} likes={post.likes} caption={post.caption} image={post.url} key={post.id}/> )}
-   </div>
-  )
+    <ResponsiveMasonry
+      columnsCountBreakPoints={{ 300: 3, 850: 4, 1400: 5 }}
+      gutterBreakpoints={{ 350: "12px", 750: "16px", 800: "18px" }}
+    >
+      <Masonry>
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            date={post.date}
+            likes={post.likes}
+            caption={post.caption}
+            image={post.url}
+          />
+        ))}
+      </Masonry>
+    </ResponsiveMasonry>
+  );
 }
