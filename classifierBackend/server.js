799cb12175eafb6d288be131379e03594a95a3ca
tayaa710 <154112253+tayaa710@users.noreply.github.com>
2025-05-03 14:20:44 +1200

    fixed stuff

--- classifierBackend/server.js
+++ classifierBackend/server.js
@@ -17,7 +17,7 @@ console.log("Client URL for CORS:", clientUrl);
 
 // Middleware
 app.use(cors({
-  origin: clientUrl,
+  origin: [clientUrl, clientUrl + '/'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
--- socket/index.js
+++ socket/index.js
@@ -12,7 +12,7 @@ const clientUrl = process.env.NODE_ENV === 'production'
 console.log("Client URL for CORS:", clientUrl);
 
 app.use(cors({
-  origin: clientUrl,
+  origin: [clientUrl, clientUrl + '/'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: clientUrl,
+    origin: [clientUrl, clientUrl + '/'],
     methods: ["GET", "POST"],
     credentials: true
   }
